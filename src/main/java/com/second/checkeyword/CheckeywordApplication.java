5e2830f484a7c823fc1e5d75da835c1dfe7acd05
cheon hwa kim <trigonotis97@gmail.com>
2020-05-06 23:56:29 +0900

    create intellij project

---
+++ src/main/java/com/second/checkeyword/CheckeywordApplication.java
@@ -0,0 +1,17 @@
+package com.second.checkeyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CheckeywordApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CheckeywordApplication.class, args);
+		System.out.println("hellow world!");
+	}
+
+
+}
---
+++ src/test/java/com/second/checkeyword/CheckeywordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second.checkeyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckeywordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
