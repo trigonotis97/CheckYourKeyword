3ac0362be0f57c7ac0751c92b97bde0fb4de4af7
Cheonhwa Kim <trigonotis97@gmail.com>
2020-06-23 22:39:22 +0900

    jsoup을 이용해서 쿠차 베스트 제품 이름 가져오기

--- src/main/java/com/second/checkeyword/JSoupSampleController.java
+++ src/main/java/com/second/checkeyword/JSoupSampleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 
-@Controller
+@RestController
 public class JSoupSampleController {
 
     @GetMapping("/sample")
@@ -20,14 +20,31 @@ public class JSoupSampleController {
         Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
         log(doc.title());
         Elements newsHeadlines = doc.select("#mp-itn b a");
+        Elements ewsHeadliness = doc.select("#mp-itn b a");
+        String bodyString="";
         for (Element headline : newsHeadlines) {
+            bodyString+="<h2>"+headline.attr("title")+"</h2> <p>"+headline.absUrl("href")+"</p>";
+
+
             log("%s\n\t%s",
                     headline.attr("title"), headline.absUrl("href"));
         }
-        return "templates/test/sample.html";
+        return bodyString;
     }
     private static void log(String msg, String... vals) {
         System.out.println(String.format(msg, vals));
     }
 
+    @GetMapping("/coocha")
+    public String coocha() throws IOException{
+        String bodyString="<h1>CooCha<h1>";
+        Document doc = Jsoup.connect("http://www.coocha.co.kr/").get();
+        log(doc.title());
+        Elements hotItems = doc.select("#dealListDiv .deal .title");
+        for(Element item : hotItems){
+            bodyString+="<p>"+item.toString()+"<p>";
+            log("%s\n",item.toString());
+        }
+        return bodyString;
+    }
 }
